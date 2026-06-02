0a54a45b27e5255f7e71f780a2c31c719c918076
fizzarizvi <fizzriz123@gmail.com>
2026-06-01 19:20:07 -0700

    feat(backend): bootstrap Spring Boot project

---
+++ backend/src/main/java/com/studyos/StudyOsApplication.java
@@ -0,0 +1,11 @@
+package com.studyos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyOsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudyOsApplication.class, args);
+    }
+}
